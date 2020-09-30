5c8de17bb3d8d3ba95899c01a87aa0ecf7baca83
Gaya3-bytes <68689904+Gaya3-bytes@users.noreply.github.com>
2020-09-30 08:58:27 +0530

    Add files via upload

---
+++ callback hell timer/script.js
@@ -0,0 +1,60 @@
+var mdiv = document.createElement('div');
+mdiv.setAttribute('class', 'disp');
+mdiv.id = "ice";
+
+
+setTimeout(function() 
+        {
+            mdiv.innerHTML = "Happy Independence Day!"}, 11000,
+            setTimeout( function()  {
+                mdiv.innerHTML = "1";
+            }, 10000, setTimeout( function()  {
+                mdiv.innerHTML = "2";
+            }, 9000,
+            setTimeout( function()  {
+                mdiv.innerHTML = "3";
+            }, 8000, 
+            setTimeout( function()  {
+                mdiv.innerHTML = "4";
+            }, 7000, 
+            setTimeout( function()  {
+                mdiv.innerHTML = "5";
+            }, 6000,
+            setTimeout( function()  {
+                mdiv.innerHTML = "6";
+            }, 5000,   setTimeout( function()  {
+                mdiv.innerHTML = "7";
+            }, 4000,
+               
+            setTimeout( function()  {
+                mdiv.innerHTML = "8";
+            }, 3000,
+            
+            setTimeout( function()  {
+                mdiv.innerHTML = "9";
+            }, 2000, 
+            
+            setTimeout( function()  {
+                mdiv.innerHTML = "10";
+            }, 1000) 
+) 
+) )) 
+) ) 
+) ) ) 
+            )
+
+
+
+
+            
+
+           
+           
+
+         
+          
+            
+         
+
+
+document.body.append(mdiv);
